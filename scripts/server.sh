fccf55716139a2f8268e27780f409198a750d3c8
Pavel Bykov <pavel.bykov@clearscale.net>
2017-10-06 17:46:47 +0700

    Added cfn wrapper for userdata. Hidden admin credentials through temp file. Added XDCR security group. Added dynamic AZ/subnet count solution based on selected parameters

--- scripts/server.sh
+++ scripts/server.sh
@@ -2,13 +2,128 @@
 
 echo "Running server.sh"
 
-adminUsername=$1
-adminPassword=$2
+license=$1
+adminUsername=$2
+adminPassword=$3
+services=$4
+stackName=$5
+
+yum -y update
+yum -y install jq
+
+if [[ $license == "None" ]]
+then
+  wget https://packages.couchbase.com/releases/5.0.0/couchbase-server-enterprise-5.0.0-centos6.x86_64.rpm
+  rpm --install couchbase-server-enterprise-5.0.0-centos6.x86_64.rpm
+fi
+
+source util.sh
+formatDataDisk
+turnOffTransparentHugepages
+setSwappinessToZero
+
+if [ -z "$6" ]
+then
+  echo "This node is part of the autoscaling group that contains the rally point."
+  rallyPublicDNS=`getRallyPublicDNS`
+else
+  rallyAutoScalingGroup=$6
+  echo "This node is not the rally point and not part of the autoscaling group that contains the rally point."
+  echo rallyAutoScalingGroup \'$rallyAutoScalingGroup\'
+  rallyPublicDNS=`getRallyPublicDNS ${rallyAutoScalingGroup}`
+fi
+
+region=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document \
+  | jq '.region'  \
+  | sed 's/^"\(.*\)"$/\1/' )
+
+instanceID=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document \
+  | jq '.instanceId' \
+  | sed 's/^"\(.*\)"$/\1/' )
+
+nodePublicDNS=`curl http://169.254.169.254/latest/meta-data/public-hostname`
 
 echo "Using the settings:"
 echo adminUsername \'$adminUsername\'
 echo adminPassword \'$adminPassword\'
+echo services \'$services\'
+echo stackName \'$stackName\'
+echo rallyPublicDNS \'$rallyPublicDNS\'
+echo region \'$region\'
+echo instanceID \'$instanceID\'
+echo nodePublicDNS \'$nodePublicDNS\'
 
-./installServer.sh
-./format.sh
-./configureServer.sh $adminUsername $adminPassword
+if [[ ${rallyPublicDNS} == ${nodePublicDNS} ]]
+then
+  aws ec2 create-tags \
+    --region ${region} \
+    --resources ${instanceID} \
+    --tags Key=Name,Value=${stackName}-ServerRally
+else
+  aws ec2 create-tags \
+    --region ${region} \
+    --resources ${instanceID} \
+    --tags Key=Name,Value=${stackName}-Server
+fi
+
+cd /opt/couchbase/bin/
+
+echo "Running couchbase-cli node-init"
+output=""
+while [[ ! $output =~ "SUCCESS" ]]
+do
+  output=`./couchbase-cli node-init \
+    --cluster=$nodePublicDNS \
+    --node-init-hostname=$nodePublicDNS \
+    --node-init-data-path=/mnt/datadisk/data \
+    --node-init-index-path=/mnt/datadisk/index \
+    --user=$adminUsername \
+    --pass=$adminPassword`
+  echo node-init output \'$output\'
+  sleep 10
+done
+
+if [[ $rallyPublicDNS == $nodePublicDNS ]]
+then
+  totalRAM=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+  dataRAM=$((50 * $totalRAM / 100000))
+  indexRAM=$((25 * $totalRAM / 100000))
+
+  echo "Running couchbase-cli cluster-init"
+  ./couchbase-cli cluster-init \
+    --cluster=$nodePublicDNS \
+    --cluster-username=$adminUsername \
+    --cluster-password=$adminPassword \
+    --cluster-ramsize=$dataRAM \
+    --cluster-index-ramsize=$indexRAM \
+    --services=${services}
+else
+  echo "Running couchbase-cli server-add"
+  output=""
+  while [[ $output != "Server $nodePublicDNS:8091 added" && ! $output =~ "Node is already part of cluster." ]]
+  do
+    output=`./couchbase-cli server-add \
+      --cluster=$rallyPublicDNS \
+      --user=$adminUsername \
+      --pass=$adminPassword \
+      --server-add=$nodePublicDNS \
+      --server-add-username=$adminUsername \
+      --server-add-password=$adminPassword \
+      --services=${services}`
+    echo server-add output \'$output\'
+    sleep 10
+  done
+
+  echo "Running couchbase-cli rebalance"
+  output=""
+  while [[ ! $output =~ "SUCCESS" ]]
+  do
+    output=`./couchbase-cli rebalance \
+    --cluster=$rallyPublicDNS \
+    --user=$adminUsername \
+    --pass=$adminPassword`
+    echo rebalance output \'$output\'
+    sleep 10
+  done
+
+fi
--- scripts/syncGateway.sh
+++ scripts/syncGateway.sh
@@ -2,5 +2,48 @@
 
 echo "Running syncGateway.sh"
 
-./installSyncGateway.sh
-./configureSyncGateway.sh
+license=$1
+stackName=$2
+
+yum -y update
+yum -y install jq
+
+if [[ $license == "None" ]]
+then
+  wget https://packages.couchbase.com/releases/couchbase-sync-gateway/1.4.1/couchbase-sync-gateway-enterprise_1.4.1-3_x86_64.rpm
+  rpm -i couchbase-sync-gateway-enterprise_1.4.1-3_x86_64.rpm
+fi
+
+region=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document \
+  | jq '.region'  \
+  | sed 's/^"\(.*\)"$/\1/' )
+
+instanceID=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document \
+  | jq '.instanceId' \
+  | sed 's/^"\(.*\)"$/\1/' )
+
+echo "Using the settings:"
+echo stackName \'$stackName\'
+echo region \'$region\'
+echo instanceID \'$instanceID\'
+
+aws ec2 create-tags \
+  --region ${region} \
+  --resources ${instanceID} \
+  --tags Key=Name,Value=${stackName}-SyncGateway
+
+file="/opt/sync_gateway/etc/sync_gateway.json"
+echo '
+{
+  "interface": "0.0.0.0:4984",
+  "adminInterface": "0.0.0.0:4985",
+  "log": ["*"]
+}
+' > ${file}
+chmod 755 ${file}
+chown sync_gateway ${file}
+chgrp sync_gateway ${file}
+
+# Need to restart to load the changes
+service sync_gateway stop
+service sync_gateway start
